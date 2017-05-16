c1c799ecbffd7913f6984418f2b02988391c9906
Thor Kell <thor@tide-pool.ca>
2017-05-15 20:32:51 -0400

    use coroutine, accept post

--- server.py
+++ server.py
@@ -9,8 +9,13 @@ Start of a server for uploading an analyzing audio with Amen
 import tornado.ioloop
 import tornado.web
 
+from tornado import gen
+
 class MainHandler(tornado.web.RequestHandler):
+    @gen.coroutine
     def get(self):
+        self.write("Hello, world")
+    def post(self):
         self.write("Hello, world")
 
 def make_app():
