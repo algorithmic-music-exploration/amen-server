c9bf4c471938d8edd83a7c15848d7bd449c09e99
Thor Kell <thor@tide-pool.ca>
2017-05-15 21:07:16 -0400

    writing uploaded file locally

--- server.py
+++ server.py
@@ -5,7 +5,6 @@
 Start of a server for uploading an analyzing audio with Amen
 '''
 
-
 import tornado.ioloop
 import tornado.web
 
@@ -16,7 +15,11 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
     def post(self):
-        self.write("Hello, world")
+        print(self.request)
+        file_body = self.request.files['file'][0]['body']
+        with open('test.mp3', 'wb') as f:
+            f.write(file_body)
+        self.write("Async done, file write may or may not be done")
 
 def make_app():
     return tornado.web.Application([
