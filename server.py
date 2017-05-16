a8cfd2a26641c273485a8a73cf4e8cb923dfd988
Thor Kell <thor@tide-pool.ca>
2017-05-15 20:16:00 -0400

    hello world

---
+++ server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Start of a server for uploading an analyzing audio with Amen
+'''
+
+
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
