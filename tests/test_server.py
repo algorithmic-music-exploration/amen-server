66655c82e76aab816ca4a69d4d68150778890615
Thor Kell <thor@tide-pool.ca>
2017-09-17 17:51:34 -0400

    test for call to enqueue passing

--- tests/test_server.py
+++ tests/test_server.py
@@ -17,7 +17,8 @@ def test_post():
     audio_filename = hash_object.hexdigest() + "-" + filename
     analysis_filename = audio_filename + '.analysis.json'
 
-    result = json.dumps({'analysis': get_url(analysis_filename), 'audio': get_url(audio_filename)})
-    # also need to check that we call upload
-    assert result == handle_post(q, files, get_url, upload) # wait,this seems to fail half the time, wtf?
+    expected = {'analysis': get_url(analysis_filename), 'audio': get_url(audio_filename)}
+    actual = json.reads(handle_post(q, files, get_url, upload))
+
     q.enqueue.assert_called_with(do_work, (ANY, audio_filename, analysis_filename, upload))
+    assert expected == actual
